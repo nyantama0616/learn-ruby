f22fa37d448e566aa6d05a0f371531b1dcd84156
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-06 20:39:34 +0900

    fizz buzzのテスト書き直した

--- basic/test/fizz_buzz_spec.rb
+++ basic/test/fizz_buzz_spec.rb
@@ -1,43 +1,34 @@
-require_relative "fizz_buzz"
+require_relative "../fizz_buzz"
 
 describe "fizz_buzz" do
   context "when x is multiple of 3" do
-    it "6" do
+    it "returns Fizz" do
       expect(fizz_buzz(6)).to eq "Fizz"
-    end
-    
-    it "9" do
       expect(fizz_buzz(9)).to eq "Fizz"
     end
   end
 
   context "when x is multiple of 5" do
-    it "10" do
+    it "returns Buzz" do
       expect(fizz_buzz(10)).to eq "Buzz"
-    end
-    
-    it "20" do
-      expect(fizz_buzz(20)).to eq "Buzz"
+      expect(fizz_buzz(25)).to eq "Buzz"
+      expect(fizz_buzz(35)).to eq "Buzz"
     end
   end
 
   context "when x is multiple of 15" do
-    it "30" do
+    it "returns FizzBuzz" do
       expect(fizz_buzz(30)).to eq "FizzBuzz"
-    end
-    
-    it "45" do
       expect(fizz_buzz(45)).to eq "FizzBuzz"
+      expect(fizz_buzz(90)).to eq "FizzBuzz"
     end
   end
 
   context "when x is not multiple of 3 nor 5" do
-    it "1" do
+    it "returns x.to_i" do
       expect(fizz_buzz(1)).to eq "1"
-    end
-
-    it "92" do
-      expect(fizz_buzz(92)).to eq "92"
+      expect(fizz_buzz(2)).to eq "2"
+      expect(fizz_buzz(19)).to eq "19"
     end
   end
 end
